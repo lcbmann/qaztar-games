ac92f733ef77437818fbe7b60de0627161d9cb9c
lcbmann <lcbmann44@gmail.com>
2023-09-09 10:46:17 -0800

    Deleting unnecessary files

--- test.mjs
+++
@@ -1,3 +0,0 @@
-export function sayHello(){
-    console.log("Text import works!")
-}
--- testimport.mjs
+++
@@ -1,3 +0,0 @@
-import { generateArrivalText } from './generate.mjs';
-var arrivalText = await generateArrivalText();
-console.log(arrivalText)
